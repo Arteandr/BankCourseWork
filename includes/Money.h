dd3c5bbc1043d1626365819175458297f1dad26a
Arteandr <artedamatik@gmail.com>
2021-05-23 12:07:05 -0400

    Add Money.h

---
+++ includes/Money.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+class Money {
+	private:
+		std::string currency; 					// Название валюты
+		long summMoney; 						// Текущий счет
+		static unsigned short count; 			// Количество экземпляров класса
+	public:
+		Money(std::string c = "$", long s = 0);
+
+		long getMoney() const;
+		const std::string getName() const;      // Метод получения названия валюты
+
+		void setName(std::string name);         // Метод установки названия валюты
+		void setSumm(long summ);                // Установка суммы денег
+
+		Money& operator +=(long summ); 			// Добавить сумму к текущей
+		Money& operator -=(long summ); 			// Отнять сумму от текущей
+		Money& operator !();                    // Метод обнуления счёта
+		Money& operator =(std::string name); 	// Установка названия валюты
+		Money& operator =(char c); 				// Установка названия валюты одним символом
+
+
+		static unsigned short getCount(); 		// Метод получения количества экземпляров класса
+
+		~Money();
+};
