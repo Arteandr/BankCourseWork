968f4b721606d6bb9e18c0f580f8c2dbb6be0661
Arteandr <artedamatik@gmail.com>
2021-06-23 18:54:18 -0400

    Fix state view

--- src/Store.cpp
+++ src/Store.cpp
@@ -36,17 +36,18 @@ void Store::initBills() {
 		this->Bills.push_back(allBills[i]);
 };
 
+
 void Store::initPremium() {
 	std::vector<std::vector<std::string>> pAcc = conf.getAccounts("PREMIUM_STATE");
 	for(int i = 0; i < pAcc.size(); i++){
 		std::vector<Money> bills;
 
 		std::vector<int> billNumbers = conf.split(pAcc[i][1]);
-		for(int i = 0; i < billNumbers.size(); i++)
-			bills.push_back(conf.getMoney(billNumbers[i]));	
+		for(int j = 0; j < billNumbers.size(); j++) {
+			bills.push_back(conf.getMoney(billNumbers[j]));	
+		};
 
 		PremiumAccount temp = PremiumAccount(pAcc[i][2],std::stol(pAcc[i][0]),bills);
-
 		this->PremiumAccounts.push_back(temp);
 	};
 };
@@ -57,8 +58,8 @@ void Store::initCommon() {
 		std::vector<Money> bills;
 
 		std::vector<int> billNumbers = conf.split(pAcc[i][1]);
-		for(int i = 0; i < billNumbers.size(); i++)
-			bills.push_back(conf.getMoney(billNumbers[i]));	
+		for(int j = 0; j < billNumbers.size(); j++)
+			bills.push_back(conf.getMoney(billNumbers[j]));	
 
 		CommonAccount temp = CommonAccount(pAcc[i][2],std::stol(pAcc[i][0]),bills);
 
@@ -73,8 +74,8 @@ void Store::initEnterprise() {
 		std::vector<Money> bills;
 
 		std::vector<int> billNumbers = conf.split(pAcc[i][1]);
-		for(int i = 0; i < billNumbers.size(); i++)
-			bills.push_back(conf.getMoney(billNumbers[i]));	
+		for(int j = 0; j < billNumbers.size(); j++)
+			bills.push_back(conf.getMoney(billNumbers[j]));	
 		
 		EnterpriseAccount temp = EnterpriseAccount(pAcc[i][2],std::stol(pAcc[i][0]),pAcc[i][3]);
 	
@@ -83,6 +84,11 @@ void Store::initEnterprise() {
 };
 
 void Store::init() {
+	this->EnterpriseAccounts.clear();
+	this->CommonAccounts.clear();
+	this->Bills.clear();
+	this->PremiumAccounts.clear();
+
 	this->initBills();
 	this->initPremium();
 	this->initCommon();
